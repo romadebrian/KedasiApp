7f84244aa1b86350ba1748b83f0b588a3dee4591
romadebrian <roma_coll04@yahoo.com>
2022-08-03 16:55:30 +0700

    first commit

---
+++ src/pages/SplashScreen/index.js
@@ -0,0 +1,14 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class SplashScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>SplashScreen</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
