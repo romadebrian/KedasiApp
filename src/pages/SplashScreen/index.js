9a9ae16b395d169b32bd1f0b600684bcc88aea0d
romadebrian <roma_coll04@yahoo.com>
2022-08-03 17:57:28 +0700

    Spalash Screen Layout

--- src/pages/SplashScreen/index.js
+++ src/pages/SplashScreen/index.js
@@ -1,14 +1,33 @@
-import { Text, StyleSheet, View } from 'react-native'
-import React, { Component } from 'react'
+import {Text, StyleSheet, View, Image} from 'react-native';
+import React, {Component} from 'react';
+
+import Logo from '../../assets/img/Logo&Name.png';
 
 export default class SplashScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>SplashScreen</Text>
+      <View style={{backgroundColor: '#FEF7EF', height: '100%'}}>
+        <View style={{alignItems: 'center'}}>
+          <Image source={Logo} style={styles.logoKedasi} />
+          <Text style={{paddingTop: 60}}>Loading 100%</Text>
+          <View
+            style={{
+              paddingTop: 13,
+              borderBottomColor: '#F5D942',
+              borderBottomWidth: 2,
+              width: 200,
+            }}
+          />
+        </View>
       </View>
-    )
+    );
   }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  logoKedasi: {
+    width: 330,
+    height: 90,
+    marginTop: 228,
+  },
+});
