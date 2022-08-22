45d9bdd58feb6c57493eac9abd7008f90c50d6ee
romadebrian <roma_coll04@yahoo.com>
2022-08-22 23:50:26 +0700

    create page TransactionList

--- src/pages/TransactionList/index.js
+++ src/pages/TransactionList/index.js
@@ -1,11 +1,38 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, BackHandler, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import CardItemOrder from "./component/CardItemOrder";
 
-const TransactionList = () => {
+const TransactionList = ({ navigation }) => {
+  const [isLoad, setIsLoad] = useState(false);
+
+  useEffect(() => {
+    if (!isLoad) {
+      setIsLoad(true);
+    }
+    const backAction = () => {
+      navigation.navigate("Dashboard");
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    );
+
+    return () => backHandler.remove();
+  });
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      setIsLoad(false);
+    });
+    return unsubscribe;
+  }, [navigation]);
+
   return (
-    <View>
-      <Text>TransactionList</Text>
-    </View>
+    <ScrollView>
+      <CardItemOrder />
+    </ScrollView>
   );
 };
 
