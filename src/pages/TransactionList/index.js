8c45f5c9c5de78e91b808b9c746e74c43c9f9368
romadebrian <roma_coll04@yahoo.com>
2022-08-26 01:03:02 +0700

    listing list transaction from table user - order

--- src/pages/TransactionList/index.js
+++ src/pages/TransactionList/index.js
@@ -1,6 +1,7 @@
-import { StyleSheet, BackHandler, ScrollView } from "react-native";
-import React, { useEffect, useState } from "react";
+import { StyleSheet, BackHandler, ScrollView, View, Text } from "react-native";
+import React, { useEffect, useState, useCallback } from "react";
 import { useSelector } from "react-redux";
+import { useFocusEffect } from "@react-navigation/native";
 import {
   getDatabase,
   ref,
@@ -13,31 +14,36 @@ import {
 } from "firebase/database";
 
 import CardItemTransaction from "./component/CardItemTransaction";
+import { async } from "@firebase/util";
 
 const TransactionList = ({ navigation }) => {
   const globalState = useSelector((state) => state.dataPengguna);
 
   const [isLoad, setIsLoad] = useState(false);
-  const [listTransaction, setListTransaction] = useState("");
+  const [listTransaction, setListTransaction] = useState([]);
 
-  useEffect(() => {
-    console.log(listTransaction);
-    if (!isLoad) {
-      handleGetListTransaction();
-      setIsLoad(true);
-    }
-    const backAction = () => {
-      navigation.navigate("Dashboard");
-      return true;
-    };
+  useFocusEffect(
+    useCallback(() => {
+      console.log(listTransaction);
+      if (!isLoad) {
+        console.log("Is Load");
+        handleGetListTransaction();
 
-    const backHandler = BackHandler.addEventListener(
-      "hardwareBackPress",
-      backAction
-    );
+        setIsLoad(true);
+      }
 
-    return () => backHandler.remove();
-  });
+      const backAction = () => {
+        navigation.navigate("Dashboard");
+        return true;
+      };
+
+      BackHandler.addEventListener("hardwareBackPress", backAction);
+
+      return () => {
+        BackHandler.removeEventListener("hardwareBackPress", backAction);
+      };
+    })
+  );
 
   useEffect(() => {
     const unsubscribe = navigation.addListener("focus", () => {
@@ -46,56 +52,89 @@ const TransactionList = ({ navigation }) => {
     return unsubscribe;
   }, [navigation]);
 
-  const handleGetListTransaction = async () => {
+  const handleGetListTransaction = () => {
     const userID = globalState.uid;
-    // const resultDatabase = []; // Must place in here for get real time data
-    const ListT = ["122", "123"];
+    var ListO = "";
 
     const db = getDatabase();
-    const orderRef = ref(db, "users/" + userID + "/order");
 
-    // try {
-    //   const ListT = [];
-    //   const dbRef = ref(getDatabase());
-    //   get(child(dbRef, `users/${userID}/order`))
-    //     .then((snapshot) => {
-    //       if (snapshot.exists()) {
-    //         Object.keys(snapshot.val()).map((key) => {
-    //           ListT.push(snapshot.val()[key].OrderId);
+    const starCountRef = ref(db, `users/${userID}/order`);
+    onValue(starCountRef, async (snapshot) => {
+      const ListT = [];
+      if (snapshot.exists()) {
+        Object.keys(snapshot.val()).map((key) => {
+          ListT.push(snapshot.val()[key].OrderId);
 
-    //           console.log(ListT);
-    //           console.log(snapshot.val()[key]);
+          // console.log(ListT);
+          console.log(snapshot.val()[key]);
+        });
 
-    //           console.log(ListT);
-    //           ListT.map((OrdId) => {
-    //             console.log("Mapping", OrdId);
+        setListTransaction(ListT);
+        ListO = ListT;
+      } else {
+        console.log("No data available");
+      }
 
-    //             const DetailOrder = query(
-    //               ref(db, "order"),
-    //               orderByChild("OrderId"),
-    //               equalTo(OrdId)
-    //             );
+      console.log(ListT);
+      return ListT;
+    });
 
-    //             get(child(DetailOrder)).then((result) => {
-    //               console.log("result", result);
-    //             });
-    //           });
-    //         });
-    //       } else {
-    //         console.log("No data available");
-    //       }
-    //     })
-    //     .catch((error) => {
-    //       console.error(error);
-    //     });
-    // } catch (error) {
-    //   console.log(error);
-    // }
+    return ListO;
   };
 
   return (
     <ScrollView>
-      <CardItemTransaction />
+      {/* {console.log(listTransaction)} */}
+      {listTransaction.length > 0
+        ? listTransaction.map((roomData) => {
+            console.log(roomData);
+            return (
+              <View key={roomData}>
+                <CardItemTransaction IDorder={roomData} />
+                {/* <Text>{roomData}</Text> */}
+              </View>
+            );
+          })
+        : null}
+
+      {/* {listTransaction.map((orderId) => {
+        console.log(orderId);
+
+        // const resultDatabase = [];
+        var dataRoom = null;
+
+        console.log(orderId);
+        const db = getDatabase();
+        const DetailOrder = query(
+          ref(db, "order"),
+          orderByChild("OrderId"),
+          equalTo(orderId)
+        );
+
+        onValue(DetailOrder, (snapshot2) => {
+          Object.keys(snapshot2.val()).map((key) => {
+            // resultDatabase.push(snapshot2.val()[key]);
+
+            dataRoom = snapshot2.val()[key];
+            console.log(snapshot2.val()[key]);
+
+            // return resultDatabase;
+          });
+          console.log("dataRoom", dataRoom?.OrderId);
+        });
+
+        // console.log("dataRoom", dataRoom?.OrderId);
+        // setListTransaction(resultDatabase);
+
+        return (
+          <View key={orderId}>
+            <CardItemTransaction />
+            <Text>{dataRoom?.Ruangan}</Text>
+          </View>
+        );
+      })} */}
+
+      {/* <CardItemTransaction /> */}
     </ScrollView>
   );
 };
