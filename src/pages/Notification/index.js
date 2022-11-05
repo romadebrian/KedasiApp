1fcc296c16b29f08d8d00364561cf4f2a481b091
romadebrian <roma_coll04@yahoo.com>
2022-11-05 16:58:19 +0700

    notification repair

--- src/pages/Notification/index.js
+++ src/pages/Notification/index.js
@@ -1,14 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, View, TouchableOpacity, ScrollView } from "react-native";
+import React from "react";
+import CardItemTransaction from "./component/CardItemNotification.js";
 
-const Notification = () => {
+const Notification = ({ navigation }) => {
   return (
-    <View>
-      <Text>Notification</Text>
-    </View>
-  )
-}
+    <ScrollView>
+      <View key={1}>
+        <CardItemTransaction
+          navigation={navigation}
+          NotificationValue="Pembayaran telah di konfirmasi"
+          DateValue="Mon, 10 Jan 2022 17:36:43 GMT"
+          Status="Unread"
+          Action="Open Page"
+        />
+      </View>
+      <View key={2}>
+        <CardItemTransaction
+          navigation={navigation}
+          NotificationValue="Anda melewati batas waktu pembayaran, pesanan anda otomatis di batalkan"
+          DateValue="Mon, 10 Jan 2022 17:36:43 GMT"
+          Status="Read"
+          Action="Open Page"
+        />
+      </View>
+      <View key={3}>
+        <CardItemTransaction
+          navigation={navigation}
+          NotificationValue="Pada hari kamis 21-02-2022 akan diadakan perbaikan pintu utama, mohon maaf jika proses perbaikan menggangu kenyamanan anda"
+          DateValue="Mon, 10 Jan 2022 17:36:43 GMT"
+          Status="Read"
+          Action="Open Page"
+        />
+      </View>
+    </ScrollView>
+  );
+};
 
-export default Notification
+export default Notification;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
