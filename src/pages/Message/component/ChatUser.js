4eda558fc03ccfc21bfd5cab890ca26aaa590ccf
romadebrian <roma_coll04@yahoo.com>
2022-11-19 07:24:52 +0700

    handle when image user not set yet

--- src/pages/Message/component/ChatUser.js
+++ src/pages/Message/component/ChatUser.js
@@ -14,38 +14,21 @@ const ChatUser = (props) => {
         <Text>{props.Data.Waktu}</Text>
         <Text style={{ fontWeight: "bold" }}>{props.Name}</Text>
       </View>
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "row",
-          alignItems: "center",
-          //   backgroundColor: "red",
-        }}
-      >
-        <Text
-          style={{
-            height: 40,
-            flex: 1,
-            borderRadius: 10,
-            color: "white",
-            backgroundColor: "#007bff",
-            textAlignVertical: "center",
-            textAlign: "right",
-            paddingRight: 5,
-          }}
-        >
-          {props.Data.Pesan}
-        </Text>
-        <Image
-          //   source={NoImage}
-          source={{ uri: props.Photo }}
-          style={{
-            width: 40,
-            height: 40,
-            borderRadius: 20,
-            marginLeft: 10,
-          }}
-        />
+      <View style={styles.rowSecond}>
+        <Text style={styles.txtPesan}>{props.Data.Pesan}</Text>
+        {props.Photo === "" ? (
+          <Image
+            //   source={NoImage}
+            source={require("../../../assets/img/no-image.png")}
+            style={styles.img}
+          />
+        ) : (
+          <Image
+            //   source={NoImage}
+            source={{ uri: props.Photo }}
+            style={styles.img}
+          />
+        )}
       </View>
     </View>
   );
@@ -53,4 +36,27 @@ const ChatUser = (props) => {
 
 export default ChatUser;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rowSecond: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    //   backgroundColor: "red",
+  },
+  txtPesan: {
+    height: 40,
+    flex: 1,
+    borderRadius: 10,
+    color: "white",
+    backgroundColor: "#007bff",
+    textAlignVertical: "center",
+    textAlign: "right",
+    paddingRight: 5,
+  },
+  img: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginLeft: 10,
+  },
+});
